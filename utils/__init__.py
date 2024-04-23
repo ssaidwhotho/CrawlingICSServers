7a9c2360b5112bb1490f813b2a52b87b087a1bdf
Leonardo Louis Parilla Gutierrez IV <65400514+ssaidwhotho@users.noreply.github.com>
2024-04-23 10:32:27 -0700

    Added similarity
    Added similarity_score into utils for similarity calculation

--- utils/__init__.py
+++ utils/__init__.py
@@ -33,3 +33,47 @@ def normalize(url):
     if url.endswith("/"):
         return url.rstrip("/")
     return url
+
+
+# Levenshtein Distance via wikipedia pseudocode https://en.wikipedia.org/wiki/Levenshtein_distance
+def levenstein_distance(url1, url2) -> int:
+    """
+    Calculate the Levenshtein distance between two urls.
+    :param url1:
+    :param url2:
+    :return: int
+    """
+    matrix = [[0 for _ in range(len(url2) + 1)] for _ in range(len(url1) + 1)]
+    for i in range(len(url1) + 1):
+        matrix[i][0] = i
+    for j in range(len(url2) + 1):
+        matrix[0][j] = j
+    for i in range(1, len(url1) + 1):
+        for j in range(1, len(url2) + 1):
+            if url1[i - 1] == url2[j - 1]:
+                cost = 0
+            else:
+                cost = 1
+            matrix[i][j] = min(matrix[i - 1][j] + 1,
+                               matrix[i][j - 1] + 1,
+                               matrix[i - 1][j - 1] + cost)
+
+    return matrix[len(url1)][len(url2)]
+
+
+def similarity_score(url1, url2) -> float:
+    """Calculate the similarity score between two URLs. via Levenshtein distance"""
+    url1 = urlparse(url1)
+    url2 = urlparse(url2)
+
+    if url1.netloc != url2.netloc:
+        return 0
+
+    path_score = levenstein_distance(url1.path, url2.path)
+    query_score = levenstein_distance(url1.query, url2.query)
+
+    max_path_length = max(len(url1.path), len(url2.path))
+    max_query_length = max(len(url1.query), len(url2.query))
+    max_distance = max(max_path_length, max_query_length)
+
+    return 1 - ((path_score + query_score) / 2) / max_distance
